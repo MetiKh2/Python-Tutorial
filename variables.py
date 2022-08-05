c82c5d42b5c3f382ac336241fea586c928c4224a
MetiKh2 <90332147+MetiKh2@users.noreply.github.com>
2022-08-05 19:26:09 +0430

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,29 @@
+fruits=["apple","banana","cherry"]
+x,y,z=fruits
+print(x+y+z)
+
+number1=8
+number2=9
+print(number1+number2)
+print(number2/number1)
+print(number1-number2)
+print(number1*number2)
+
+#x=str(3) # x will be '3'
+#y=int(3)# y will be 3
+#z=float(3)# z will be 3.0
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+name1="Mahdi"
+# is the same as
+name2='Mahdi'
+
+a=4
+A="Mahdi"
+#A will not overwrite a
+
+print(a,A)
+
