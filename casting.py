45d96fe7da9d8f3c47e05eeb46915397204667c2
MetiKh2 <90332147+MetiKh2@users.noreply.github.com>
2022-08-07 21:48:01 +0430

    Create casting.py

---
+++ casting.py
@@ -0,0 +1,28 @@
+int1=int(1) # will be 1
+int2=int(2.8) #  will be 2
+int3=int("3") #  will be 3
+
+
+float1=float(1) # will be 1.0
+float2=float(2.8) # will be 2.8
+float3=float("3") # will be 3.0
+float4=float('4.2') # will be 4.2
+
+str1=str('s1') # will be 's1'
+str2=str(2) # will be '2'
+str3=str(4.5) # will be '4.5'
+
+print(int1)
+print(int2)
+print(int3)
+
+print(str1)
+print(str2)
+print(str3)
+
+print(float1)
+print(float2)
+print(float3)
+print(float4)
+
+ 
