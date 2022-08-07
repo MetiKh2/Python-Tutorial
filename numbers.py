316bf98706386bec0a2f96fe0c20611c90bd0cd9
MetiKh2 <90332147+MetiKh2@users.noreply.github.com>
2022-08-07 21:38:32 +0430

    Create numbers.py

---
+++ numbers.py
@@ -0,0 +1,51 @@
+import random
+
+print(random.randrange(1,100))
+
+x=1 #int
+y=2.8 #float
+z=2j #complex
+
+#convert from int to float 
+a=float(x)
+
+#convert from float to int
+b=int(y)
+
+#convert from int to complex
+c=complex(x)
+
+print(type(a),a)
+print(type(b),b)
+print(type(c),c)
+
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+# int1=1
+# int2=3537483947294
+# int3=-243748384
+
+# print(type(int1))
+# print(type(int2))
+# print(type(int3))
+
+# f1=35e3
+# f2=1.0
+# f3=-35.59
+
+# print(type(f1))
+# print(type(f2))
+# print(type(f3))
+
+# print(f1)
+
+# c1=3+5j
+# c2=5j
+# c3=-5j
+
+# print(type(c1))
+# print(type(c2))
+# print(type(c3))
