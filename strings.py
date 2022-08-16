a06a215cdc1104de95d0fe33face6eb788135d1c
MetiKh2 <90332147+MetiKh2@users.noreply.github.com>
2022-08-16 20:23:55 +0430

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,9 @@
+age=36
+txt="My age is : {}"
+print(txt.format(age)) 
+
+quantity=3
+price =49.95
+id=10
+order="I want to pay {2} dollars for {0} pieces of item {1}"
+print(order.format(quantity,id,price)) 
