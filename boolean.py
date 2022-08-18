7b1bd6d21bbce84f6395e155ecaa013119da72a9
MetiKh2 <90332147+MetiKh2@users.noreply.github.com>
2022-08-18 14:14:12 +0430

    Create boolean.py

---
+++ boolean.py
@@ -0,0 +1,26 @@
+# print(10>9)
+# print(10==9)
+# print(10<9)
+
+# a=200
+# b=30
+
+# if b>a:
+#     print('b is greater than a')
+# else :
+#     print('a is greater than b')
+
+# x='hello'
+# y=15
+
+# print(bool(x))
+# print(bool(y))
+# print(bool(['apple','banana']))
+
+print(bool(()))
+print(bool([]))
+print(bool({}))
+print(bool(""))
+print(bool(0))
+print(bool(None))
+print(bool(False))
