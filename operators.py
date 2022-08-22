e676be9ae15a748b397500bd661a0ae5b298c56c
MetiKh2 <90332147+MetiKh2@users.noreply.github.com>
2022-08-22 20:22:36 +0430

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,43 @@
+# x=5
+# y=2
+
+# print(x+y)
+# print(x-y)
+# print(x*y)
+# print(x/y)
+# print(x%y)
+# print(x**y)
+# print(x//y)
+
+# x+=3 #x=x+3
+# print(x)
+# x-=3 #x=x-3
+# print(x)
+# x*=3 #x=x*3
+# print(x)
+# x/=3 #x=x/3
+# print(x)
+# x%=3 #x=x%3
+# print(x)
+# x//=3 #x=x//3
+# print(x)
+# x**=3 #x=x**3
+# print(x)
+
+# print(x==y)
+# print(x!=y)
+# print(x>y)
+# print(x<y)
+# print(x>=y)
+# print(x<=y)
+
+# print(x<6 and x<10)
+# print(x<6 or x<4)
+# print(not(x<5 or x<4))
+
+x=[1,2,3]
+y=[1,2,3]
+z=x
+# print(x is not z)
+print(1 in x)
+print(5 not in x)
