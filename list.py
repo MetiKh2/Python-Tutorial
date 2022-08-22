69b46e5f14afe479f3a86c2fe4d82942526bb13e
MetiKh2 <90332147+MetiKh2@users.noreply.github.com>
2022-08-22 21:56:33 +0430

    Create list.py

---
+++ list.py
@@ -0,0 +1,18 @@
+myList1=['apple','banana','cherry']
+myList2=list((1,4,8))
+myList3=[True,False,False]
+myList4=['abc',34,True,3,'hello']
+# print(myList1)
+# print(myList2)
+# print(myList3)
+# print(type(myList4))
+
+# print(myList1[-4:-1])
+
+# if 'apple' in myList1:
+#     print('Yes')
+# else :
+#     print('No')
+
+myList1[1:3]=['blackcurrant']
+print(myList1)
