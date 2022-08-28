c1a9bd157d016670a37868db5e984e2d1fea6cc6
MetiKh2 <90332147+MetiKh2@users.noreply.github.com>
2022-08-28 19:27:24 +0430

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,10 @@
+#pt1
+myTuple=('apple','banana','cherry','kiwi','melon','mango')
+# myList=list(myTuple)
+# myList.append('orange')
+# myTuple2=('orange',)
+# myTuple+=myTuple2
+# myList.remove('apple')
+# myTuple=tuple(myList)
+del myTuple
+print(myTuple)
