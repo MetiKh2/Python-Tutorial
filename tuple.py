ec33b28d69323361e55cdc8d77304aeaa972bf3c
MetiKh2 <90332147+MetiKh2@users.noreply.github.com>
2022-08-30 18:54:21 +0430

    Update tuple.py

--- tuple.py
+++ tuple.py
@@ -8,3 +8,20 @@ myTuple=('apple','banana','cherry','kiwi','melon','mango')
 # myTuple=tuple(myList)
 del myTuple
 print(myTuple)
+
+#pt2
+fruits=('apple','mango','papaya','pineapple','cherry')
+numbers=(1,2,3,4)
+# (green,*tropic,red)=fruits
+# print(green)
+# print(tropic)
+# print(red)
+# for x in fruits:
+#     print(x)
+# for i in range(len(fruits)):
+#     print(fruits[i])
+# i=0
+# while i<len(fruits):
+#     print(fruits[i])
+#     i+=1
+print(fruits*2)
