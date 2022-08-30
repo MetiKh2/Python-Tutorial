3494c6ac53dd9d218f6f0525f18702b86662aa8d
MetiKh2 <90332147+MetiKh2@users.noreply.github.com>
2022-08-31 00:43:08 +0430

    Update Dictionaries.py

--- Dictionaries.py
+++ Dictionaries.py
@@ -26,3 +26,43 @@ thisdict['color']='black'
 print(thisdict)
  
  
+#pt2
+
+car={
+    'brand':'Ford',
+    'model':'Mustang',
+    'year':1964
+}
+
+# car.popitem()
+# del car['brand']
+# del car
+# car.clear()
+#dict2=dict1
+
+# for x, y in car.items():
+    # print(x, y)
+
+# my_dict=car.copy()
+# my_dict=dict(car)
+
+# print(my_dict)
+child1={
+        'name':'Emil',
+        'year':2004
+    }
+child2={
+        'name':'Tbias',
+        'year':2007
+    }
+child3={
+         'name':'Linus',
+        'year':2011
+    }
+myfamily={
+    'child1' :child1,
+    'child2' :child2,
+    'child3' :child3,
+     
+}
+print(myfamily)
