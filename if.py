a9f352db2642e33475eac0ba98cc5046fc24096b
MetiKh2 <90332147+MetiKh2@users.noreply.github.com>
2022-09-06 16:07:24 +0430

    Create if.py

---
+++ if.py
@@ -0,0 +1,29 @@
+a=200
+b=20
+c=500
+x=15
+# if b>a:
+#    print('b is greater than a')
+# # elif a==b:
+# #     print('a and b are equal')
+# else :
+#     print('b is not greater than a')
+
+# if a>b:print('a is greater than b')
+
+# print('a')if a>b else print('b')
+# print('a')if a>b else print('=') if a==b else print('b')
+
+# if a>b and c<a:
+#     print('Both conditions are true')
+# if a>b or c<a:
+#     print('At least one condition is true')
+# if x>10:
+#     print('Above ten,')
+#     if x>20:
+#         print('and also above 20')
+#     else :
+#         print('but not above 20')
+
+if a>b:
+    pass
