7dc7532e03059a1e672b42432563921ddd4f864e
MetiKh2 <90332147+MetiKh2@users.noreply.github.com>
2022-09-07 23:24:45 +0430

    Create function.py

---
+++ function.py
@@ -0,0 +1,30 @@
+# def my_function(**kid):
+#     print('His last name is '+kid['lname'])
+
+# def my_function(country='Norway'):
+#     print('I am from '+country)
+# def my_function(food):
+#     for x in food:
+#         print(x)
+# def my_function(number):
+#     return number+5
+# def my_function(number):
+#     pass
+
+ 
+
+# my_function(fname='Tobias',lname='Refsnes')
+# my_function('Sweden')
+# my_function('India')
+# my_function()
+# my_function('Brazil')
+
+# fruits=['apple','banana','cherry']
+# my_function(fruits)
+
+# x =my_function(9)
+# print(x)
+
+ 
+ 
+ 
