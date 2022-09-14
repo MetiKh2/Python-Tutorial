8e3bf41a38b95e269c022a77d2d54b306b2795ea
MetiKh2 <90332147+MetiKh2@users.noreply.github.com>
2022-09-14 11:35:32 +0430

    Create even_odd_positive_negative.py

---
+++ even_odd_positive_negative.py
@@ -0,0 +1,20 @@
+#Even Or Odd ??
+# number=int(input("PLease Enter a Number : "))
+
+# if number % 2==0:
+#     print('Even')
+# else :
+#     print('Odd')
+
+#Positive Or Negative ?? 
+
+number=int(input("PLease Enter a Number : "))
+
+if number>0:
+    print('Positive')
+elif number<0:
+    print('Negative')
+else :
+    print(0)
+
+
