44595cdf142cd75cde68cb08a69326137be92ac0
MetiKh2 <90332147+MetiKh2@users.noreply.github.com>
2022-09-14 14:29:42 +0430

    Create return_print.py

---
+++ return_print.py
@@ -0,0 +1,14 @@
+#print
+def sum(x,y):
+    print(x+y)
+sum(8,29)
+#print
+def welcome(name):
+    print('welcome '+name)
+welcome('John')
+
+#return
+def sum2(x,y):
+    return x+y
+result=sum2(10,10)
+print(result)
